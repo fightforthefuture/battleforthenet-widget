2d6617efa4158eb1bcb76baade2bc3496724d023
Paul Buonopane <Zenexer@users.noreply.github.com>
2017-07-12 04:17:43 -0400

    Fix #93: Dialog keeps appearing on page loads
    Cookie needs `path=/` set, or it only applies to the current directory and subdirectories.

--- widget.js
+++ widget.js
@@ -89,7 +89,7 @@
       d.setTime(d.getTime()+(exdays*24*60*60*1000));
 
       var expires = "expires="+d.toGMTString();
-      document.cookie = name + "=" + val + "; " + expires;
+      document.cookie = name + "=" + val + "; " + expires + "; path=/";
     },
 
     getCookie: function(cname) {

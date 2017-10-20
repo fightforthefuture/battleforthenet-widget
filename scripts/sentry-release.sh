2ad5ef7a12200e2577453ea3695752fb231df47c
Mike Morris <mikemorris@users.noreply.github.com>
2017-10-20 02:45:00 -0400

    fix bash syntax for sentry-release.sh

--- scripts/sentry-release.sh
+++ scripts/sentry-release.sh
@@ -5,4 +5,4 @@ set -e
 curl https://sentry.io/api/hooks/release/builtin/187468/${SENTRY_WEBHOOK}/ \
   -X POST \
   -H 'Content-Type: application/json' \
-  -d '{"version": "`git rev-parse HEAD`"}'
+  -d '{"version": "'`git rev-parse HEAD`'"}'

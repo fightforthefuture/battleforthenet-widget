b2568b380f44fc59948e2b9398e679a1a1608636
Mike Morris <mikemorris@users.noreply.github.com>
2017-10-20 02:16:19 -0400

    use Sentry webhook for release tracking with commit hashes

---
+++ scripts/sentry-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+curl https://sentry.io/api/hooks/release/builtin/187468/${SENTRY_WEBHOOK}/ \
+  -X POST \
+  -H 'Content-Type: application/json' \
+  -d '{"version": "`git rev-parse HEAD`"}'

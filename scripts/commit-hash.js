47e3e68e4094b2f4d4fef4ef013c127f1fbdaefc
Mike Morris <mikemorris@users.noreply.github.com>
2017-10-25 15:31:14 -0400

    fix commitHash function def

--- scripts/commit-hash.js
+++ scripts/commit-hash.js
@@ -3,7 +3,7 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-module.exports = async commitHash() => {
+module.exports = async function commitHash() {
   const { stdout, stderr } = await exec('git rev-parse HEAD');
   return stdout.trim();
 };
